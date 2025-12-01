3d0e1b378a3178039a54f7bce5696cac3b9c17ae
Emil <emilbabazade29@gmail.com>
2025-12-01 23:02:59 +0200

    movement

--- main.lua
+++ main.lua
@@ -3,15 +3,45 @@ if arg[2] == "debug" then
 end
 
 function love.load()
-    Image = love.graphics.newImage('monkey.png')
+    Width, Height = love.graphics.getDimensions()
+    Len = 50
+    X = Width / 2 - Len / 2
+    Y = Height / 2 - Len / 2
+    Speed = 300
 end
 
-function love.update()
-
+function love.update(dt)
+    movement(dt)
 end
 
 function love.draw()
-    love.graphics.draw(Image, love.math.random(0, 800), love.math.random(0, 600))
+    love.graphics.rectangle('line', X, Y, Len, Len)
+end
+
+function movement(dt)
+    if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
+        Y = Y + Speed * dt
+        if Y > Height + Len / 2 then
+            Y = -Len
+        end
+    elseif love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+        Y = Y - Speed * dt
+        if Y < -Len / 2 then
+            Y = Height + Len
+        end
+    end
+        
+    if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+        X = X - Speed * dt
+        if X < -Len / 2 then
+            X = Width + Len
+        end
+    elseif love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+        X = X + Speed * dt
+        if X > Width + Len / 2 then
+            X = Len
+        end
+    end
 end
 
 local love_errorhandler = love.errorhandler
