153fdf31b5280c2b822384a093626f7575239226
Emil <emilbabazade29@gmail.com>
2025-12-01 21:51:35 +0200

    skeleton for any love2d project

---
+++ main.lua
@@ -0,0 +1,25 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+function love.load()
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
+
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
