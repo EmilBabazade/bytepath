a24df7b69c32d73aadfb900b1704c1aca4a2bf6a
Emil <emilbabazade29@gmail.com>
2025-12-01 23:23:42 +0200

    tables

--- main.lua
+++ main.lua
@@ -3,19 +3,36 @@ if arg[2] == "debug" then
 end
 
 function love.load()
-    Width, Height = love.graphics.getDimensions()
-    Len = 50
-    X = Width / 2 - Len / 2
-    Y = Height / 2 - Len / 2
-    Speed = 300
+    -- Width, Height = love.graphics.getDimensions()
+    -- Len = 50
+    -- X = Width / 2 - Len / 2
+    -- Y = Height / 2 - Len / 2
+    -- Speed = 300
+    ------
+    -- Fruits = {'apple', 'banana'}
+    -- for i = 1, #Fruits do
+    --     print(Fruits[i])
+    -- end
+    ------
+    Books = {
+        createBook("Bob in bobland", "bob", 23),
+        createBook("Tom in bobland the long awaited sequel", "bob", 50),
+        createBook("Geremy in bobland the failed triqual", "bob", 67),
+    }
 end
 
 function love.update(dt)
-    movement(dt)
+    --table.insert(Fruits, "monkey")
+    -- movement(dt)
 end
 
 function love.draw()
-    love.graphics.rectangle('line', X, Y, Len, Len)
+    -- love.graphics.rectangle('line', X, Y, Len, Len)
+    for i,v in ipairs(Books) do
+        love.graphics.print(v.title, 100, 100 + 50 * i)
+        love.graphics.print(v.author, 500, 100 + 50 * i)
+        love.graphics.print(v.pages, 600, 100 + 50 * i)
+    end
 end
 
 function movement(dt)
@@ -42,6 +59,14 @@ function movement(dt)
             X = Len
         end
     end
+end
+
+function createBook(title, author, pages) 
+    return {
+        title=title, 
+        author=author, 
+        pages=pages
+    }
 end
 
 local love_errorhandler = love.errorhandler
